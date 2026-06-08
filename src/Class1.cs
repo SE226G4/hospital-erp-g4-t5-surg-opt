8174ef159b5ab03690f044a76030f4e533850fad
Maryam Alhamwi <maryamalhamwi03@gmail.com>
2026-06-08 23:58:01 +0300

    feat: add surgery logic and tests

---
+++ src/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace HospitalERP_SurgOPT.src{
+public class SurgeryManager{
+public bool ValidateSurgeryScheduling(bool isPatientValid, bool isTeamAvailable, bool isToolsReady, bool isBedAvailable, bool isSterilized)
+{
+    if (isPatientValid) 
+    {
+        if (isTeamAvailable) 
+        {
+            if (isToolsReady) 
+            {
+                if (isBedAvailable) 
+                {
+                    if (isSterilized) 
+                    {
+                        return true; 
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
+}
+}
